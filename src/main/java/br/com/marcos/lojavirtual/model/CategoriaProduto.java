2cdbbced5f1950de857920c02d20b6219d6c7d1d
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-02 15:11:02 -0300

    Criando entity de categoria produto

--- src/main/java/br/com/marcos/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/br/com/marcos/lojavirtual/model/CategoriaProduto.java
@@ -15,14 +15,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto") 
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produtoo") 
     private Long id;
 
     private String nomeDesc;
 }
-
