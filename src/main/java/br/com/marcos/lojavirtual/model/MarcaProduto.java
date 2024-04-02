1ca158736e8f258e9012ac31fd494a304be69046
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-02 12:21:47 -0300

    Criando tabela no banco para teste

---
+++ src/main/java/br/com/marcos/lojavirtual/model/MarcaProduto.java
@@ -0,0 +1,28 @@
+package br.com.marcos.lojavirtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto") // Corrigido o nome do gerador de sequência
+    private Long id;
+
+    private String nomeDesc;
+}
+
