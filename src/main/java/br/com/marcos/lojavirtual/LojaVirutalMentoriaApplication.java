3bc7c126af86b71906c3db94f31002578b1ff413
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-01 21:37:22 -0300

    Iniciando

---
+++ src/main/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirutalMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirutalMentoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirutalMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
