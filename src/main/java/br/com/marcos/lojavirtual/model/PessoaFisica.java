5a147520faf5b5cd58c576d7de18de56bc0c7c6b
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-03 14:09:44 -0300

    Entity de pessoa fisica

---
+++ src/main/java/br/com/marcos/lojavirtual/model/PessoaFisica.java
@@ -0,0 +1,30 @@
+package br.com.marcos.lojavirtual.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends Pessoa{
+
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+			
+}
