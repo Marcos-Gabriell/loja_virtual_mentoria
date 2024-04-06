cff6baaf02138fbda24e905faf025985d2b0d50d
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-06 14:33:12 -0300

    Entity de nota fiscal produto

---
+++ src/main/java/br/com/marcos/lojavirtual/model/NotaItemProduto.java
@@ -0,0 +1,44 @@
+package br.com.marcos.lojavirtual.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "nota_item_produto")
+@SequenceGenerator(name = "seq_nota_item_produto", sequenceName = "seq_nota_item_produto", allocationSize = 1, initialValue = 1)
+public class NotaItemProduto {
+
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_item_produto")
+	@Id
+	private Long id;
+	
+	private Double quantidade;
+	
+	@ManyToOne
+	@JoinColumn(name = "nota_fiscal_compra_id", nullable = false,
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_compra_fk"))
+	private NotaFiscalCompra notaFiscalCompra;
+	
+	@ManyToOne
+	@JoinColumn(name = "produto_id", nullable = false,
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_id"))
+	private Produto produto;
+}
