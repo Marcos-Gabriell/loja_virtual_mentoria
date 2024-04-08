01c3bce58416bae0dd908546d07c37b75908d53b
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-08 20:56:14 -0300

    Anotações na class princial

--- src/main/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplication.java
+++ src/main/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplication.java
@@ -2,8 +2,16 @@ package br.com.marcos.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = " br.com.marcos.lojavirtual.model")
+@ComponentScan(basePackages = {"br.com.marcos.*"})
+@EnableJpaRepositories(basePackages = {"br.com.marcos.lojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirutalMentoriaApplication {
 
 	public static void main(String[] args) {
