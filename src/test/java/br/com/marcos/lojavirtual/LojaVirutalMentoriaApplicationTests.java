1aae2ee8267ebe9cccee44097b5ab2ba11288da0
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-17 15:34:21 -0300

    Testando persistencia no banco

--- src/test/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplicationTests.java
+++ src/test/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplicationTests.java
@@ -1,13 +1,30 @@
 package br.com.marcos.lojavirtual;
 
+import br.com.marcos.lojavirtual.model.Acesso;
+import br.com.marcos.lojavirtual.repository.AcessoRepository;
+import br.com.marcos.lojavirtual.service.AcessoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LojaVirutalMentoriaApplication.class)
 class LojaVirutalMentoriaApplicationTests {
 
+
+	@Autowired
+	private AcessoService service;
+
+	@Autowired
+	private AcessoRepository repository;
+
 	@Test
-	void contextLoads() {
+	public void testCadastraAcesso() {
+
+		Acesso acesso = new Acesso();
+
+		acesso.setDescricao("ROLE_ADMIN");
+
+		repository.save(acesso);
 	}
 
 }
