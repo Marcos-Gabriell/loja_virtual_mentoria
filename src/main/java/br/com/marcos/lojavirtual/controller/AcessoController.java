50a799cdb1248a15426f52cf35bea81a0e0cec17
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-04-20 22:27:51 -0300

    Avaçando na arquitetura rest

--- src/main/java/br/com/marcos/lojavirtual/controller/AcessoController.java
+++ src/main/java/br/com/marcos/lojavirtual/controller/AcessoController.java
@@ -3,8 +3,13 @@ package br.com.marcos.lojavirtual.controller;
 import br.com.marcos.lojavirtual.model.Acesso;
 import br.com.marcos.lojavirtual.service.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -14,8 +19,13 @@ public class AcessoController {
     @Autowired
     private AcessoService service;
 
-    @PutMapping("/salvarAcesso")
-    public Acesso salvarAcesso(Acesso acesso) {
-        return service.save(acesso);
+    @ResponseBody
+    @PostMapping(value = "**/salvarAcesso")
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
+    	
+    	Acesso acesoSalvo = service.save(acesso);
+    	
+    	return new ResponseEntity<Acesso>(acesoSalvo, HttpStatus.OK);
+    
     }
 }
