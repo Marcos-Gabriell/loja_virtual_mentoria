0e132239afa888da6ed7c85db66e50a3d5c2497e
mgabr <mgabr@host.docker.internal>
2024-05-01 14:02:50 -0300

    finalizando repository de consultar usuario no banco

--- src/main/java/br/com/marcos/lojavirtual/repository/UsuarioRepository.java
+++ src/main/java/br/com/marcos/lojavirtual/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.com.marcos.lojavirtual.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import br.com.marcos.lojavirtual.model.Usuario;
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
 
+	@Query(value = "select u from Usuario u where u.login = ?1")
+	Usuario findByLogin(String login);
 }
