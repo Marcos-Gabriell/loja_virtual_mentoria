70e7c09ecae188b371a35025799888c87c3aef8e
mgabr <mgabr@host.docker.internal>
2024-05-02 11:10:11 -0300

    Criando token com jwt

---
+++ src/main/java/br/com/marcos/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -0,0 +1,40 @@
+package br.com.marcos.lojavirtual.security;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.crypto.Data;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+@Component
+public class JWTTokenAutenticacaoService {
+
+	private static final long EXPIRATION_TME = 959990000;
+
+	private static final String SECRET = "ss/-*-*sds565dsd-s/d-s*dsds";
+
+	private static final String TOKEN_PREFIX = "Bearer";
+
+	private static final String HEADER_STRING = "Authorization";
+	
+	public void addAuthentication(HttpServletResponse response, String username) throws Exception {
+		
+		String jWT = Jwts.builder().
+				setSubject(username)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TME))
+				.signWith(SignatureAlgorithm.HS512, SECRET).compact();
+		
+		String token = TOKEN_PREFIX + " " + jWT;
+		
+		response.addHeader(HEADER_STRING, token);
+		
+		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+
+	}
+}
--- src/main/java/br/com/marcos/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/br/com/marcos/lojavirtual/security/WebConfigSecurity.java
@@ -2,18 +2,32 @@ package br.com.marcos.lojavirtual.security;
 
 import javax.servlet.http.HttpSessionListener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.marcos.lojavirtual.service.ImplementacaoUserDatailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener{
 
+	@Autowired
+	private ImplementacaoUserDatailsService implementacaoUserDatailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(implementacaoUserDatailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso")
