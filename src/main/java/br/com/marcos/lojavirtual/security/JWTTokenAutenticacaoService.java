90bdea68cc4f0b0243d4dec0f4dabdb4e440c0ce
mgabr <mgabr@host.docker.internal>
2024-05-03 13:10:15 -0300

    TEste

--- src/main/java/br/com/marcos/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/br/com/marcos/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -1,9 +1,10 @@
 package br.com.marcos.lojavirtual.security;
 
+import static java.util.Objects.isNull;
+
 import java.util.Date;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.crypto.Data;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -37,4 +38,21 @@ public class JWTTokenAutenticacaoService {
 		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
 
 	}
+	
+	
+	// CORS policy
+		private void liberarCorsPolicy(HttpServletResponse response) {
+			if (isNull(response.getHeader("Access-Control-Allow-Origin"))) {
+				response.addHeader("Access-Control-Allow-Origin", "*");		
+			}
+			if (isNull(response.getHeader("Access-Control-Allow-Headers"))) {
+				response.addHeader("Access-Control-Allow-Headers", "*");		
+			}
+			if (isNull(response.getHeader("Access-Control-Request-Headers"))) {
+				response.addHeader("Access-Control-Request-Headers", "*");		
+			}
+			if (isNull(response.getHeader("Access-Control-Allow-Methods"))) {
+				response.addHeader("Access-Control-Allow-Methods", "*");		
+			}
+		}
 }
