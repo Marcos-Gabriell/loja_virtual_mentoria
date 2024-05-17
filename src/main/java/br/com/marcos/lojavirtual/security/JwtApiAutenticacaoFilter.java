4119d4bd82621b9046abe6a8df95c9d7eac46c18
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-05-17 16:07:37 -0300

    erro

--- src/main/java/br/com/marcos/lojavirtual/security/JwtApiAutenticacaoFilter.java
+++ src/main/java/br/com/marcos/lojavirtual/security/JwtApiAutenticacaoFilter.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.ServletResponse;import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class JwtApiAutenticacaoFilter extends GenericFilterBean{
@@ -15,6 +16,9 @@ public class JwtApiAutenticacaoFilter extends GenericFilterBean{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
+		
+		Authentication authentication = new JWTTokenAutenticacaoService()
+				.getAuthentication((HttpServletRequest))
 	}
 
 }
