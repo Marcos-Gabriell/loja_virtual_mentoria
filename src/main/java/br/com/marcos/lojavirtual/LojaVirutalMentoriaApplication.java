1f0a1765c5b6860ed387d219ba655736edfd9251
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-05-28 08:31:25 -0300

    Validação de falhar de login

--- src/main/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplication.java
+++ src/main/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplication.java
@@ -17,7 +17,6 @@ public class LojaVirutalMentoriaApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println(new BCryptPasswordEncoder().encode("admin"));
 		
 		SpringApplication.run(LojaVirutalMentoriaApplication.class, args);
 	}
