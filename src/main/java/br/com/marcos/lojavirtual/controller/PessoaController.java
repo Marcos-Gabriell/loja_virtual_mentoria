0da7855096aea157bd5de63b052f3fcd64025e2a
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-06-10 10:17:08 -0300

    Erro

--- src/main/java/br/com/marcos/lojavirtual/controller/PessoaController.java
+++ src/main/java/br/com/marcos/lojavirtual/controller/PessoaController.java
@@ -13,30 +13,33 @@ import br.com.marcos.lojavirtual.model.PessoaJuridica;
 import br.com.marcos.lojavirtual.repository.PessoaRepository;
 import br.com.marcos.lojavirtual.service.PessoaUserService;
 
+
+
 @RestController
 public class PessoaController {
-
+	
 	@Autowired
-	private PessoaRepository pessoaRepository;
+	private PessoaRepository pesssoaRepository;
 	
 	@Autowired
 	private PessoaUserService pessoaUserService;
 	
+	/*end-point é microsservicos é um API*/
 	@ResponseBody
-	@PostMapping(value = "**/salvarpj")
-	public ResponseEntity<PessoaJuridica> salvarPj(@RequestBody PessoaJuridica pessoaJuridica) throws ExceptionMentoriaJava {
+	@PostMapping(value = "**/salvarPj")
+	public ResponseEntity<PessoaJuridica> salvarPj(@RequestBody PessoaJuridica pessoaJuridica) throws ExceptionMentoriaJava{
 		
 		if (pessoaJuridica == null) {
-			throw new ExceptionMentoriaJava("Pessoa jurídica não pode ser NULL! ");
+			throw new ExceptionMentoriaJava("Pessoa juridica nao pode ser NULL");
 		}
 		
-		if (pessoaJuridica.getId() == null && pessoaRepository.existeCnpjCadastrado(pessoaJuridica.getCnpj()) != null) {
+		if (pessoaJuridica.getId() == null && pesssoaRepository.existeCnpjCadastrado(pessoaJuridica.getCnpj()) != null) {
 			throw new ExceptionMentoriaJava("Já existe CNPJ cadastrado com o número: " + pessoaJuridica.getCnpj());
 		}
 		
 		pessoaJuridica = pessoaUserService.salvarPessoaJuridica(pessoaJuridica);
 		
-		
 		return new ResponseEntity<PessoaJuridica>(pessoaJuridica, HttpStatus.OK);
 	}
+
 }
